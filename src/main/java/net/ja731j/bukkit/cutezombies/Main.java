763907c47df05fd2c85f7ce673d441090373c15e
jetkiwi <jetkiwi@gmail.com>
2014-07-27 17:32:52 +0900

    Other mobs are also stronger

--- src/main/java/net/ja731j/bukkit/cutezombies/Main.java
+++ src/main/java/net/ja731j/bukkit/cutezombies/Main.java
@@ -11,11 +11,16 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
 import org.bukkit.scheduler.BukkitScheduler;
 
 /**
@@ -50,7 +55,8 @@ public class Main extends JavaPlugin implements Listener {
                     effects.add(new PotionEffect(PotionEffectType.SPEED, 1200, 1));
                     le.addPotionEffects(effects);
                 }
-            } else {
+            }
+            if (Math.random() < 0.10) {
                 final Item i = w.dropItem(l, new ItemStack(Material.EXP_BOTTLE));
 
                 BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
@@ -61,7 +67,54 @@ public class Main extends JavaPlugin implements Listener {
                             i.remove();
                         }
                     }
-                }, 45L);
+                }, 25L);
+            }
+        }
+        if (e.getEntity().getType() == EntityType.SKELETON) {
+            World w = e.getEntity().getWorld();
+            Location l = e.getEntity().getLocation();
+
+            if (Math.random() > 0.5) {
+                LivingEntity le = (LivingEntity) w.spawnEntity(l, EntityType.CREEPER);
+                List<PotionEffect> effects = new ArrayList<PotionEffect>();
+                effects.add(new PotionEffect(PotionEffectType.INVISIBILITY, 10, 1));
+
+                if (Math.random() > 0.5) {
+                    effects.add(new PotionEffect(PotionEffectType.SPEED, 1200, 1));
+                    le.addPotionEffects(effects);
+                }
+            }
+            if (Math.random() < 0.10) {
+                ItemStack s = new Potion(PotionType.NIGHT_VISION).toItemStack(1);
+
+                
+                final Item i = w.dropItem(l, s);
+
+                BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+                scheduler.scheduleSyncDelayedTask(this, new Runnable() {
+                    @Override
+                    public void run() {
+                        if (i.isValid()) {
+                            i.remove();
+                        }
+                    }
+                }, 25L);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onCreatureSpawn(CreatureSpawnEvent e) {
+        if (e.getSpawnReason() != SpawnReason.CUSTOM) {
+            switch (e.getEntityType()) {
+                case ZOMBIE:
+                case SKELETON:
+                case SPIDER:
+                    e.getEntity().setMaxHealth(30);
+                    e.getEntity().setHealth(30);
+                    break;
+                default:
+                    break;
             }
         }
     }
